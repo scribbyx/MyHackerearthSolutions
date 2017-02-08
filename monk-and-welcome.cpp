6af5f8b1b60d205bfd4a8487b634616eb4f32c08
scribbyx <scribbyx@gmail.com>
2017-02-08 20:36:33 +0530

    monks

---
+++ monk-and-welcome.cpp
@@ -0,0 +1,24 @@
+include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+
+	cin>>n;
+	int a[n], b[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}	
+	for(int i=0;i<n;i++)
+	{
+		cin>>b[i];
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		b[i]+=a[i];
+		cout<<b[i]<<" ";
+	}
+}
